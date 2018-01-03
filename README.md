e162e344669b79b2f9cd145cda1c614aae787134
Yoshitake Hamano <oed0cow6oy5@gmail.com>
2018-01-03 19:19:47 +0900

    Implement Neo Pixel sample

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # yudetamago-esd-wroom-02
+
+
+## Library
+
+- Adafruit NeoPixel Library (LGPL v3.0)
+  https://github.com/adafruit/Adafruit_NeoPixel
--- src/main.ino
+++ src/main.ino
@@ -1,12 +1,28 @@
-#define LED_PIN 13
+#include <Adafruit_NeoPixel.h>
+
+#define LED_PIN           13
+#define NEO_PIXEL_PIN     5
+#define NUM_OF_NEO_PIXELS 1
+
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUM_OF_NEO_PIXELS,
+                                             NEO_PIXEL_PIN,
+                                             NEO_GRB + NEO_KHZ800);
 
 void setup() {
+    pixels.begin();
+    pixels.setBrightness(255);
+
     pinMode(LED_PIN, OUTPUT);
 }
 
 void loop() {
+    pixels.setPixelColor(0, 255, 0, 0);
+    pixels.show();
     digitalWrite(LED_PIN, HIGH);
     delay(1000);
+
+    pixels.setPixelColor(0, 0, 255, 0);
+    pixels.show();
     digitalWrite(LED_PIN, LOW);
     delay(1000);
 }
